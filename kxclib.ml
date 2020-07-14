215c699f6308dfff4878affb3cee4a91b5490eea
Haochen Xie <haochenx@acm.org>
2020-07-14 09:07:50 +0900

    kxclib - add Queue

--- kxclib.ml
+++ kxclib.ml
@@ -44,6 +44,22 @@ module Either = struct
 end
 type ('a, 'b) either = ('a, 'b) Either.t
 
+module Queue : sig
+  type 'x t
+  val empty : 'x t
+  val add : 'x t -> 'x -> 'x t
+  val take : 'x t -> ('x * 'x t) option
+end = struct
+  type 'x t = 'x list*'x list
+  let empty = [], []
+  let add (r,u) x = (x :: r, u)
+  let rec take (r,u) = match u, r with
+    | hd :: rest, _ -> Some (hd, (r, rest))
+    | [], (_ :: _) -> take ([], List.rev r)
+    | [], [] -> None
+end
+type 'x queue = 'x Queue.t
+
 module List = struct
   include List
 
