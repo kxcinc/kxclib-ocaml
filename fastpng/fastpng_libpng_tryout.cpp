3089c1f4278f6fc8a7242a9c8bb49bd3be903773
Haochen Xie <haochenx@acm.org>
2020-10-13 00:10:10 +0900

    add kxclib.fastpng

---
+++ fastpng/fastpng_libpng_tryout.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+namespace kxclib_fastpng {
+  int write_png_rgba(const char *filename, int width, int height, int32_t *buffer);
+}
+
+int main(int argc, char *argv[])
+{
+    int width = 9;
+    int height = 12;
+    
+    int32_t *buff = new int32_t[width*height];
+    for (int x = 0; x < width; ++x)
+      for (int y = 0; y < height; ++y) {
+        int32_t color;
+        if (x == y) {
+          color = 0xffcccccc;
+        } else if (y % 3 == 0) {
+          color = 0xffff0000;
+        } else if (y % 3 == 1) {
+          color = 0xff00ff00;
+        } else if (y % 3 == 2) {
+          color = 0xff0000ff;
+        }
+        buff[x*height+y] = color;
+      }
+
+  return kxclib_fastpng::write_png_rgba("test.png", width, height, buff);
+}
+
+// hack to make CMake happy
+extern "C" {
+size_t caml_string_length() {
+  return -1;
+}
+}
