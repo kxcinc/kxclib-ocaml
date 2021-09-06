31b0fcada5e6e813505eedff256dd94cc5021c90
Ryusuke Narita <nr@kxc.io>
2021-09-06 16:22:34 +0900

    kxclib_test - add seq iteri

--- test_nr/kxclib_gen_unit_tests.ml
+++ test_nr/kxclib_gen_unit_tests.ml
@@ -8,10 +8,29 @@ let trivial () =
   check string "I'm trivial" "hello" "hello"
 
 
+let seq_iteri n msgs () =
+  let open Seq in
+  let s = iota n in
+  let an_array = Array.make n "" in
+  let mk_msg i x = sprintf "Hello %d.%d!" i x in
+  let f i x =
+    an_array.(i) <- mk_msg i x in
+  let expected = msgs in
+  let actual = iteri f s; an_array in
+  check (array string) "seq_iteri" actual expected
+
+
+let seq_iteri_0 =
+  seq_iteri 3 [|"Hello 0.0!"; "Hello 1.1!"; "Hello 2.2!"|]
+
+
 let () =
   Printexc.record_backtrace true;
   run "Datecode_unit_tests" [
       "trivial", [
         test_case "trivial_case" `Quick trivial
+      ];
+      "seq_iteri", [
+          test_case "seq_iteri_0" `Quick seq_iteri_0
       ]
     ]
