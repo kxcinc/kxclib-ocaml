f3cc27a051cdcf6b272255a2b178772ae8a149db
Haochen Kotoi-Xie <haochenx@acm.org>
2023-02-25 00:42:20 +0900

    fix jsoo/unit_test for ocaml.5.0.0

--- jsoo/unit_test/additional_stubs.js
+++ jsoo/unit_test/additional_stubs.js
@@ -3,3 +3,9 @@
 function unix_gethostname() {
   return caml_string_of_jsstring("js");
 }
+
+//Provides: caml_unix_gethostname
+//Requires: caml_string_of_jsstring
+function caml_unix_gethostname() {
+  return caml_string_of_jsstring("js");
+}
