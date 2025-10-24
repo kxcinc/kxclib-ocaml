a009171aa691744cdd348de731c8c1ab4d7c098b
kxc-takejio <tj@kxc.inc>
2025-10-25 02:16:55 +0900

    Add propety for invalid utf-8

--- unit_test/baselib_json_jcsnafi_prop_tests.ml
+++ unit_test/baselib_json_jcsnafi_prop_tests.ml
@@ -1,17 +1,42 @@
-let min_fi_float = Float.of_int (- (1 lsl 52)) ;;
-let max_fi_float = (Float.of_int ((1 lsl 52) - 1)) ;;
 let min_fi_int = (- (1 lsl 52)) ;;
 let max_fi_int = (1 lsl 52) - 1 ;;
 
 let invalid_neg_int_range = (QCheck2.Gen.int_range min_int (pred min_fi_int))
 let invalid_pos_int_range = (QCheck2.Gen.int_range (succ max_fi_int) max_int)
 
-let is_exn exn (f : unit -> unit) : bool =
+let gen_random_byte_string =
+  QCheck2.Gen.map String.of_list (QCheck2.Gen.list_size (QCheck2.Gen.int_range 1 100) QCheck2.Gen.char)
+
+let is_invalid_utf8 str =
+  let str_len = String.length str in
+  let rec loop i =
+    if i >= str_len then false
+    else
+      let utf8dec = String.get_utf_8_uchar str i in
+        if Uchar.utf_decode_is_valid utf8dec then
+          loop (i + Uchar.utf_decode_length utf8dec)
+        else
+          true
+  in
+    loop 0
+
+let is_exn exn (f : unit -> 'a) : bool =
   try
-    f ();
+    ignore (f ());
     false
   with
   | e when e = exn -> true
+  | _ -> false
+
+let is_exn_p (p : exn -> bool) (f : unit -> 'a) : bool =
+  try
+    ignore (f ());
+    false
+  with
+  | e -> p e
+
+let is_invalid_arg_prefix_invalid_unicode = function
+  | Invalid_argument msg -> String.starts_with "Invalid Unicode:" msg
   | _ -> false
 
 let () =
@@ -33,11 +58,22 @@ let () =
     that "unparse_jcsnafi: Invalid negative integer range" invalid_neg_int_range ~print:string_of_int
       (fun i ->
         is_exn (Invalid_argument "float or out-of-range integer")
-          (fun () -> ignore (Json_JCSnafi.unparse_jcsnafi(`num (float_of_int i))))
+          (fun () -> Json_JCSnafi.unparse_jcsnafi(`num (float_of_int i)))
       );
     that "unparse_jcsnafi: Invalid positive integer range" invalid_pos_int_range ~print:string_of_int
       (fun i ->
         is_exn (Invalid_argument "float or out-of-range integer")
-          (fun () -> ignore (Json_JCSnafi.unparse_jcsnafi(`num (float_of_int i))))
-      );    
+          (fun () -> Json_JCSnafi.unparse_jcsnafi(`num (float_of_int i)))
+      );
+    that "unparse_jcsnafi: Invalid UTF-8 string" gen_random_byte_string ~print:identity
+      (fun s ->
+        QCheck2.assume (is_invalid_utf8 s);
+        is_exn_p is_invalid_arg_prefix_invalid_unicode
+          (fun () -> Json_JCSnafi.unparse_jcsnafi (`str s)));
+    that "unparse_jcsnafi: Invalid UTF-8 object property name" gen_random_byte_string ~print:identity
+      (fun s ->
+        QCheck2.assume (is_invalid_utf8 s);
+        is_exn_p is_invalid_arg_prefix_invalid_unicode
+          (fun () -> Json_JCSnafi.unparse_jcsnafi (`obj [(s, `null)])));
+          (* TODO JSON.jv generator*)
   ] |> run_tests |> exit
