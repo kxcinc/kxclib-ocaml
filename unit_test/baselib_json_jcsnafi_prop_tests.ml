5d5ab481521902d3ed25739ba42d6dd4b654d8ac
kxc-takejio <tj@kxc.inc>
2025-11-10 04:40:57 +0900

    refactoring

--- unit_test/baselib_json_jcsnafi_prop_tests.ml
+++ unit_test/baselib_json_jcsnafi_prop_tests.ml
@@ -1,10 +1,10 @@
 open Kxclib_priv_test_lib.Json
 open Kxclib.Json
 
-let min_fi_float = -. (2. ** 52.)
-let max_fi_float = (2. ** 52.) -. 1.
-let min_fi_int53p = Int53p.of_float min_fi_float
-let max_fi_int53p = Int53p.of_float max_fi_float
+let min_fi_float : float = -. (2. ** 52.)
+let max_fi_float : float = (2. ** 52.) -. 1.
+let min_fi : int53p = Int53p.of_float min_fi_float
+let max_fi : int53p = Int53p.of_float max_fi_float
 
 (* Generators *)
 
@@ -31,47 +31,45 @@ let gen_int64_range (low_i64 : int64) (high_i64 : int64) : int64 QCheck2.Gen.t =
           Int64.add low_i64 offset
         ) QCheck2.Gen.int64
 
-let gen_fi_int53p : int53p QCheck2.Gen.t = 
-  let min_fi_int53p_int64 = Int53p.to_int64 min_fi_int53p in
-  let max_fi_int53p_int64 = Int53p.to_int64 max_fi_int53p in
+let gen_fi : int53p QCheck2.Gen.t = 
+  let min_fi_int53p_int64 = Int53p.to_int64 min_fi in
+  let max_fi_int53p_int64 = Int53p.to_int64 max_fi in
   QCheck2.Gen.map Int53p.of_int64 (gen_int64_range min_fi_int53p_int64 max_fi_int53p_int64)
 
-let invalid_neg_fi_int64 = gen_int64_range Int64.min_int (Int64.pred (Int53p.to_int64 min_fi_int53p))
-let invalid_pos_fi_int64 = gen_int64_range (Int64.succ (Int53p.to_int64 max_fi_int53p)) Int64.max_int
+let invalid_neg_fi_int64 = gen_int64_range Int64.min_int (Int64.pred (Int53p.to_int64 min_fi))
+let invalid_pos_fi_int64 = gen_int64_range (Int64.succ (Int53p.to_int64 max_fi)) Int64.max_int
 
-let gen_invalid_utf8_char = QCheck2.Gen.(
+let gen_invalid_utf8_char : char QCheck2.Gen.t = QCheck2.Gen.(
   oneof [
     char_range (char_of_int 128) (char_of_int 191);
     char_range (char_of_int 192) (char_of_int 193);
     char_range (char_of_int 245) (char_of_int 255);
   ])
 
-let gen_random_invalid_utf8_string = QCheck2.Gen.(
+let gen_random_invalid_utf8_string : string QCheck2.Gen.t = QCheck2.Gen.(
   map String.of_list (list_size (int_range 1 100) gen_invalid_utf8_char)
 )
 
-let gen_unicode_char =
-  QCheck2.Gen.(
+let gen_unicode_char : Uchar.t QCheck2.Gen.t = QCheck2.Gen.(
     let gen_unicode_codepoint = oneof [int_range 0x0000 0xD7FF; int_range 0xE000 0x10FFFF] in
-    map Uchar.of_int gen_unicode_codepoint
-  )
+    map Uchar.of_int gen_unicode_codepoint)
 
-let gen_uchar_list len =
-  QCheck2.Gen.(
+let gen_uchar_list (len : int) : Uchar.t list QCheck2.Gen.t = QCheck2.Gen.(
     int_range 0 len >>= fun len' ->
-    list_size (pure len') gen_unicode_char
-  )
+    list_size (pure len') gen_unicode_char)
 
-let string_of_uchars uchars =
+let string_of_uchars (uchars : Uchar.t list) : string =
   let buf = Buffer.create (List.length uchars) in
   List.iter (Buffer.add_utf_8_uchar buf) uchars;
   Buffer.contents buf
 
-let gen_unicode_string_len len = QCheck2.Gen.map string_of_uchars (gen_uchar_list len)
+let gen_unicode_string_len (len : int) : string QCheck2.Gen.t =
+  QCheck2.Gen.map string_of_uchars (gen_uchar_list len)
 
-let gen_unicode_string = gen_unicode_string_len 10
+let gen_unicode_string : string QCheck2.Gen.t =
+  gen_unicode_string_len 10
 
-let gen_lone_surrogate_string =
+let gen_lone_surrogate_string : string QCheck2.Gen.t =
   let gen_surrogate_codepoint = QCheck2.Gen.int_range 0xD800 0xDFFF in
   let bytes_of_surrogate_codepoint code =
     (* | 1110xxxx | ________ | ________ | *)
@@ -86,34 +84,35 @@ let gen_lone_surrogate_string =
                    | 2 -> b3
                    | _ -> assert false)
   in
-    QCheck2.Gen.map bytes_of_surrogate_codepoint gen_surrogate_codepoint
+  QCheck2.Gen.map bytes_of_surrogate_codepoint gen_surrogate_codepoint
 
-let gen_unicode_string_with_surrogate_len len = QCheck2.Gen.(
+let gen_unicode_string_with_surrogate_len (len : int) : string QCheck2.Gen.t = QCheck2.Gen.(
   map3 (fun prefix invalid suffix -> prefix ^ invalid ^ suffix)
        (gen_unicode_string_len len)
        gen_lone_surrogate_string
        (gen_unicode_string_len len))
 
-let gen_unicode_string_with_surrogate = gen_unicode_string_with_surrogate_len 10
+let gen_unicode_string_with_surrogate : string QCheck2.Gen.t =
+  gen_unicode_string_with_surrogate_len 10
 
-let gen_unicode_string_with_invalid_unicode_string_len len = QCheck2.Gen.(
+let gen_unicode_string_with_invalid_unicode_string_len (len : int) : string QCheck2.Gen.t = QCheck2.Gen.(
   map3 (fun prefix invalid suffix -> prefix ^ invalid ^ suffix)
        (gen_unicode_string_len len)
        gen_random_invalid_utf8_string
        (gen_unicode_string_len len))
 
-let gen_unicode_string_with_invalid_unicode_string = gen_unicode_string_with_invalid_unicode_string_len 10
+let gen_unicode_string_with_invalid_unicode_string : string QCheck2.Gen.t =
+  gen_unicode_string_with_invalid_unicode_string_len 10
 
-let gen_unique_fnames len = QCheck2.Gen.(
+let gen_unique_fnames (len : int) : string list QCheck2.Gen.t = QCheck2.Gen.(
   let module StringSet = Set.Make(String) in
   map (fun l -> StringSet.of_list l |> StringSet.elements)
       (list_repeat len gen_unicode_string))
 
-let rec sized_jv size = QCheck2.Gen.(
+let rec sized_jv (size : int) : jv QCheck2.Gen.t = QCheck2.Gen.(
   let gen_null = pure `null in
   let gen_bool = bool >|= (fun x -> `bool x) in
-  let gen_int = gen_fi_int53p >|= (fun x -> `num (Int53p.to_float x)) in
-  let gen_num = gen_int in
+  let gen_num = gen_fi >|= (fun x -> `num (Int53p.to_float x)) in
   let gen_str = gen_unicode_string >|= (fun s -> `str s) in
   let gen_atom = oneof [ gen_null; gen_bool; gen_num; gen_str; ] in
 
@@ -148,9 +147,10 @@ let rec sized_jv size = QCheck2.Gen.(
                     (5, gen_obj size);
                    ])
 
-let gen_jv_jcsnafi = QCheck2.Gen.sized sized_jv
+let gen_jv_jcsnafi : jv QCheck2.Gen.t =
+  QCheck2.Gen.sized sized_jv
 
-let rec gen_shuffled_jv jv = QCheck2.Gen.(
+let rec gen_shuffled_jv (jv : jv) : jv QCheck2.Gen.t = QCheck2.Gen.(
   match jv with
   | `obj es ->
       shuffle_l es >>= (fun shuffled_es ->
@@ -162,12 +162,12 @@ let rec gen_shuffled_jv jv = QCheck2.Gen.(
       flatten_l shuffled_xs >|= (fun xs' -> `arr xs')
   | _ -> pure jv)
 
-let gen_jv_and_shuffled_pair = QCheck2.Gen.(
+let gen_jv_and_shuffled_pair : (jv * jv) QCheck2.Gen.t = QCheck2.Gen.(
   gen_jv_jcsnafi >>= fun jv ->
   gen_shuffled_jv jv >>= fun shuffled_jv ->
   pure (jv, shuffled_jv))
 
-let gen_jv_with_duplicate_keys = QCheck2.Gen.(sized (fun size ->
+let gen_jv_with_duplicate_keys : jv QCheck2.Gen.t = QCheck2.Gen.(sized (fun size ->
     (* n is number of object keys *)
     let n = max 2 size in
     int_range 1 (max 1 (n / 2)) >>= fun len ->
@@ -199,14 +199,14 @@ let rec contains_str_or_obj (jv : jv) : bool =
       || List.exists (fun (_, v) -> contains_str_or_obj v) es
   | _ -> false
 
-let rec filter_gen p gen = QCheck2.Gen.(
+let rec filter_gen (p : 'a -> bool) (gen : 'a QCheck2.Gen.t) : 'a QCheck2.Gen.t = QCheck2.Gen.(
   gen >>= fun v -> if p v then pure v
-                   else filter_gen p gen
-)
+                   else filter_gen p gen)
 
-let gen_jv_with_str_or_obj = filter_gen contains_str_or_obj gen_jv_jcsnafi
+let gen_jv_with_str_or_obj : jv QCheck2.Gen.t =
+  filter_gen contains_str_or_obj gen_jv_jcsnafi
 
-let rec gen_inject_one_bad_string bad_string_gen jv = QCheck2.Gen.(
+let rec gen_inject_one_bad_string (bad_string_gen : string QCheck2.Gen.t) (jv : jv) : jv QCheck2.Gen.t = QCheck2.Gen.(
   match jv with
   | `str _ ->
       bad_string_gen >|= (fun s -> `str s)
@@ -260,11 +260,11 @@ let rec gen_inject_one_bad_string bad_string_gen jv = QCheck2.Gen.(
   | _ -> pure jv
 )
 
-let gen_jv_with_invalid_unicode = QCheck2.Gen.(
+let gen_jv_with_invalid_unicode : jv QCheck2.Gen.t = QCheck2.Gen.(
   no_shrink gen_jv_with_str_or_obj >>=
     gen_inject_one_bad_string gen_unicode_string_with_invalid_unicode_string)
 
-let gen_jv_with_surrogates = QCheck2.Gen.(
+let gen_jv_with_surrogates : jv QCheck2.Gen.t = QCheck2.Gen.(
   no_shrink gen_jv_with_str_or_obj >>=
     gen_inject_one_bad_string gen_unicode_string_with_surrogate)
 
@@ -285,7 +285,7 @@ let does_throw_p (p : exn -> bool) (f : unit -> 'a) : bool =
   with
   | e -> p e
 
-let is_invalid_utf8 str =
+let is_invalid_utf8 str : bool =
   let str_len = String.length str in
   let rec loop i =
     if i >= str_len then false
@@ -298,22 +298,22 @@ let is_invalid_utf8 str =
   in
     loop 0
 
-let is_invalid_arg_prefix prefix = function
+let is_invalid_arg_prefix prefix : exn -> bool = function
   | Invalid_argument msg -> String.starts_with prefix msg
   | _ -> false
 
-let is_whitespace = function
+let is_whitespace : char -> bool = function
  | ' ' | '\t' | '\n' | '\r'  -> true
  | _ -> false
 
-let count_preceding_backslashes s i =
+let count_preceding_backslashes s i : int =
   let rec count j cnt = 
     if j < 0 || s.[j] <> '\\' then cnt
     else count (j - 1) (cnt + 1)
 in
   count (i - 1) 0
 
-let has_extra_whitespace_outside_string s =
+let has_extra_whitespace_outside_string s : bool =
   let len = String.length s in
   let rec loop i in_string = 
     if i >= len then false
