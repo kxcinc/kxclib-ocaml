78de9e8ed9f8985a9524b950ecc9ff816d71f6b2
kxc-wraikny <wk@kxc.inc>
2025-11-10 19:37:36 +0900

    refine code

--- melange/tests/jcsnafi_test.ml
+++ melange/tests/jcsnafi_test.ml
@@ -3,65 +3,49 @@ open Kxclib_js
 open Utils
 open Jest
 
-module SP = struct
+(* test_data/jcsnafi_test_data.ml *)
+module Jcsnafi_data = Jcsnafi_test_data.Make(struct
   let succ f = f +. 1.0
   let pred f = f -. 1.0
-end
-  
-let test_unparse_jcsnafi () =
-  let case jv fmt = fun () ->
-    let unparsed = Json_JCSnafi.unparse_jcsnafi jv in
-    to_be (expect unparsed) fmt;
-    Promise_io.return ()
-  in
+end)
 
-  let case_exn jv (exn: exn) = fun () ->
+open struct
+  let case actual (expected) = fun () ->
+    to_be (expect actual) expected;
+    Promise_io.return ()
+end
+
+let test_unparse_jcsnafi () =
+  let case_exn f (exn: exn) = fun () ->
     let error_name = match exn with
       | Invalid_argument _ -> "Invalid_argument"
       | _ -> "Error"
     in
-    to_throw (expect (fun () -> Json_JCSnafi.unparse_jcsnafi jv)) error_name;
+    to_throw (expect (f ())) error_name;
     Promise_io.return ()
   in
 
-  Jcsnafi_test_data.cases_for_unparse_jcsnafi (module SP) case case_exn
+  Jcsnafi_data.cases_for_unparse_jcsnafi case case_exn
   |> fold_promise
 
 let test_is_encodable () =
-  let case jv (expected: bool) = fun () ->
-    to_be (expect (Json_JCSnafi.is_encodable jv)) expected;
-    Promise_io.return ()
-  in
-  
-  Jcsnafi_test_data.cases_for_is_encodable (module SP) case
+  Jcsnafi_data.cases_for_is_encodable case
   |> fold_promise
 
 let test_is_encodable_str () =
-  let case s (expected: bool) = fun () ->
-    to_be (expect (Json_JCSnafi.is_encodable_str s)) expected;
-    Promise_io.return ()
-  in
-  Jcsnafi_test_data.cases_for_is_encodable_str case
+  Jcsnafi_data.cases_for_is_encodable_str case
   |> fold_promise
 
 let test_is_encodable_num () =
-  let case f (expected: bool) = fun () ->
-    to_be (expect (Json_JCSnafi.is_encodable_num f)) expected;
-    Promise_io.return ()
-  in
-  Jcsnafi_test_data.cases_for_is_encodable_num (module SP) case
+  Jcsnafi_data.cases_for_is_encodable_num case
   |> fold_promise
 
 let test_compare_field_name () =
-  let case str1 str2 (expected: int) = fun () ->
-    to_be (expect (Json_JCSnafi.compare_field_name str1 str2)) expected;
-    Promise_io.return ()
-  in
-  Jcsnafi_test_data.cases_for_compare_field_name case
+  Jcsnafi_data.cases_for_compare_field_name case
   |> fold_promise
 
 let test_compare_field_name_rfc8785 () =
-  Jcsnafi_test_data.test_compare_field_name_rfc8785 &
+  Jcsnafi_data.test_compare_field_name_rfc8785 &
     (fun ~actual ~expected () ->
       to_strict_equal (expect actual) expected;
       Promise_io.return ())
